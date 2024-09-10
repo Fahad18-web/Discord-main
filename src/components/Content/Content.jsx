dbed6ba7e1aae515b0f03064755b99e4ed3f41d2
Fahad <fa2367701@gmail.comgit config  --global user.email fa2367701@gmail.com>
2024-09-10 10:38:29 +0500

    added animations

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import './Content.css'
 const Content = () => {
   return (
     <>
       <div className="bg-black text-white p-3 sm:flex">
         <Row className="sm:flex items-center justify-center ml-10">
           <Col md={6} className="sm:flex items-center flex-col p-3 max-sm:ml-10 ">
-            <h1 className="display-6 fw-bold text-uppercase mb-4 fs-1 w-[280px]">
+            <h1 className="fade-in display-6 fw-bold text-uppercase mb-4 fs-1 w-[280px]">
               Group chat that’s all fun & games
             </h1>
-            <p className="lead text-align-justify w-[280px]">
+            <p className="slide-in lead text-align-justify w-[280px]">
               Discord is great for playing games and chilling with friends, or even building a worldwide community. Customize your own space to talk, play, and hang out.
             </p>
           </Col>
